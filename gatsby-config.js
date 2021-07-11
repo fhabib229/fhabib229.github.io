b6d39b51d1cc925eb2b878f10a08cbfdfb24a51c
Faris Habib <faris.habib@protonmail.ch>
2021-07-10 17:36:14 -0700

    U

--- gatsby-config.js
+++ gatsby-config.js
@@ -146,7 +146,7 @@ module.exports = {
         background_color: `#292a2d`,
         theme_color: `#292a2d`,
         display: `minimal-ui`,
-        icon: `src/images/B.png`,
+        icon: `src/images/U.png`,
       },
     },
     {
